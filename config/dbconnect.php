bcbdf3ff5ba4b25ba25cf42176a7fdddcb8ef993
praveenmenon <Abhi@Praveens-MBP.fios-router.home>
2018-09-07 07:59:54 -0400

    [master development]fixed database connection error syntax

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -3,6 +3,6 @@
   $connection = new mysqli($servername, $username, $password, $dbname);
 
   if ($connection -> connect_error) {
-    die("Database connection failed:" . $conn->connect_error);
+    die("Database connection failed:" . $connection->connect_error);
   }
 ?>
